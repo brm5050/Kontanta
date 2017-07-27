3870043e86fc5c58ac259a9cac8b6e94464788b9
Winston Lum <meow@Winstons-MacBook-Pro.local>
2017-07-27 19:11:37 -0400

    spacing fixes

--- models/petDetails.js
+++ models/petDetails.js
@@ -1,97 +1,87 @@
-
-var Sequelize = require('sequelize');
-//var sequelize = require
+var Sequelize = require("sequelize");
 
 module.exports = function(sequelize, DataTypes) {
 
-var petDetails = sequelize.define('petDetails', {
-petName : {
-  type : Sequelize.STRING,
-  allowNull: false,
-
-  validate: {
+  var petDetails = sequelize.define("petDetails", {
+    petName : {
+      type : Sequelize.STRING,
+      allowNull: false,
+      validate: {
         len: [1, 10]
       }
-	},
-	
-petDescription : {
-  type : Sequelize.STRING,
-  allowNull: false,
-
-  validate: {
+    },
+    	
+    petDescription : {
+      type : Sequelize.STRING,
+      allowNull: false,
+      validate: {
         len: [1, 151]
       }
-	},
+    },
 
-age : {
-  type : Sequelize.INTEGER,
-  allowNull: false,
-
-  validate: {
+    age : {
+      type : Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
         len: [1, 2]
       }
-  },
-  gender : {
-  type : Sequelize.STRING,
-  allowNull: false,
+    },
 
-  validate: {
+    gender : {
+      type : Sequelize.STRING,
+      allowNull: false,
+      validate: {
         len: [1, 10]
       }
-  },
+    },
 
 
-price : {
-  type : Sequelize.INTEGER,
-  allowNull: false,
-
-  validate: {
+    price : {
+      type : Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
         len: [1, 11]
       }
-	},
+  	},
 
-stock : {
-  type : Sequelize.BOOLEAN,
-  allowNull: false,
-
-  validate: {
+    stock : {
+      type : Sequelize.BOOLEAN,
+      allowNull: false,
+      validate: {
         len: [1]
       }
-	},
+  	},
+
     img: {
-    type: Sequelize.BLOB,
-    allowNull: true
-  },
+      type: Sequelize.BLOB,
+      allowNull: true
+    },
+
     vaccinated: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  });
 
+  petDetails.associate = function(models) {
+      // We're saying that a petDetails should belong to pets
+      // A petDetails can't be created without an pet due to the foreign key constraint
+      petDetails.belongsTo(models.pets, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+  };
 
-});
-
-
-petDetails.associate = function(models) {
-    // We're saying that a petDetails should belong to pets
-    // A petDetails can't be created without an pet due to the foreign key constraint
-    petDetails.belongsTo(models.pets, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-};
-
-petDetails.associate = function(models) {
+  petDetails.associate = function(models) {
     // Associating pets with petDetails
     // When an pets is deleted, also delete any associated petDetails
     petDetails.hasOne(models.purchaseDetails, {
       onDelete: "cascade"
     });
-};
+  };
 
-
-return petDetails;
-
+  return petDetails;
 };
 
 
--- models/purchaseDetails.js
+++ models/purchaseDetails.js
@@ -1,118 +1,103 @@
-
 var Sequelize = require('sequelize');
 //var sequelize = require
 
 module.exports = function(sequelize, DataTypes) {
 
-var purchaseDetails = sequelize.define('purchaseDetails', {
-firstName : {
-  type : Sequelize.STRING,
-  allowNull: false,
-
-  validate: {
+  var purchaseDetails = sequelize.define('purchaseDetails', {
+    firstName : {
+      type : Sequelize.STRING,
+      allowNull: false,
+      validate: {
         len: [1, 10]
       }
-	},
+    },
 
-lastName : {
-  type : Sequelize.STRING,
-  allowNull: false,
-
-  validate: {
+    lastName : {
+      type : Sequelize.STRING,
+      allowNull: false,
+      validate: {
         len: [1, 10]
       }
-  },
-	
+    },
 
-ccNumber : {
-  type : Sequelize.INTEGER,
-  allowNull: false,
-
-  validate: {
+    ccNumber : {
+      type : Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
         len: [1]
       }
-  },
+    },
 
-cvv : {
-  type : Sequelize.INTEGER,
-  allowNull: false,
-
-  validate: {
+    cvv : {
+      type : Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
         len: [1, 3]
       }
-  },
+    },
 
-  mmyy : {
-  type : Sequelize.INTEGER,
-  allowNull: false,
-
-  validate: {
+    mmyy : {
+      type : Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
         len: [1, 4]
       }
-  },
+    },
 
-  address : {
-  type : Sequelize.STRING,
-  allowNull: false,
-
-  validate: {
+    address : {
+      type : Sequelize.STRING,
+      allowNull: false,
+      validate: {
         len: [1, 10]
       }
-  },
+    },
 
-  city : {
-  type : Sequelize.STRING,
-  allowNull: false,
-
-  validate: {
+    city : {
+      type : Sequelize.STRING,
+      allowNull: false,
+      validate: {
         len: [1, 10]
       }
-  },
+    },
 
-  state : {
-  type : Sequelize.STRING,
-  allowNull: false,
-
-  validate: {
+    state : {
+      type : Sequelize.STRING,
+      allowNull: false,
+      validate: {
         len: [1, 10]
       }
-  },
+    },
 
 
-zipcode : {
-  type : Sequelize.INTEGER,
-  allowNull: false,
-
-  validate: {
+    zipcode : {
+      type : Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
         len: [1, 5]
       }
-	},
+    },
 
-price : {
-  type : Sequelize.INTEGER,
-  allowNull: false,
-
-  validate: {
+    price : {
+      type : Sequelize.INTEGER,
+      allowNull: false,
+      validate: {
         len: [1, 5]
       }
-  }
+    }
+  });
 
 
-});
-
-
-purchaseDetails.associate = function(models) {
-    // We're saying that a petDetails should belong to pets
-    // A petDetails can't be created without an pet due to the foreign key constraint
+  purchaseDetails.associate = function(models) {
+      // We're saying that a petDetails should belong to pets
+      // A petDetails can't be created without an pet due to the foreign key constraint
     purchaseDetails.belongsTo(models.petDetails, {
       foreignKey: {
         allowNull: false
       }
     });
-};
+  };
 
-return purchaseDetails;
-
+  return purchaseDetails;
 };
 
 
